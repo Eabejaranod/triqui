f17f7a834114bc036dc7aca5ee352bfb14515da9
alejandro bejarano <eabejaranod@correo.udistrital.edu.co>
2023-02-28 19:03:11 -0500

    v.3

---
+++ juego.py
@@ -0,0 +1,10 @@
+from jugador import Jugador
+from tablero import Tablero
+
+class juego():
+    def init(self):
+        self.jugador=Jugador
+        self.tablero=Tablero
+        self.turno='x'
+    def iniciar_juego():
+        
